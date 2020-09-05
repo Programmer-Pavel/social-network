4bae2d31cdf1fd39f33291f8870528cfc45ba661
Pavel <pavelkukuskmn388@gmail.com>
2020-09-05 19:46:24 +0300

    added my project social network

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://miro.medium.com/max/2400/1*3MveTPrDuM_v_Fx0y1FJmg.png" alt=""/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://palakmishra.files.wordpress.com/2017/06/cropped-image.png?w=676" alt=""/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New posts
+            </div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
