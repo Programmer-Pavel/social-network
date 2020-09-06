515a52b8de01b5a00d8c5edabfb1db83028c3426
Pavel <pavelkukuskmn388@gmail.com>
2020-09-06 21:08:34 +0300

    Posts, the grouping of components, folder structure

---
+++ src/components/Profile/My posts/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from "./Post.module.css";
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src="https://i.pinimg.com/736x/5e/b3/a5/5eb3a5665fe4e63f8c813a525aa3beb8.jpg" alt=""/>
+            post 1
+        </div>
+    )
+}
+
+export default Post;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from "./Profile.module.css";
+import MyPosts from "./My posts/MyPosts";
 
 const Profile = () => {
     return <div className={s.content}>
@@ -9,17 +10,7 @@ const Profile = () => {
         <div>
             ava + description
         </div>
-        <div>
-            My posts
-            <div>
-                New posts
-            </div>
-            <div className={s.posts}>
-                <div className={s.item}>post 1</div>
-                <div className={s.item}>post 2</div>
-                <div className={s.item}>post 3</div>
-            </div>
-        </div>
+        <MyPosts />
     </div>
 }
 
