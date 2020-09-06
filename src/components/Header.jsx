fd856f63eb328e983ae11699b21855eac757f525
Pavel <pavelkukuskmn388@gmail.com>
2020-09-06 14:19:15 +0300

    added css modules "name.module.css"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
-    return <header className='header'>
+    return <header className={s.header}>
         <img src="https://miro.medium.com/max/2400/1*3MveTPrDuM_v_Fx0y1FJmg.png" alt=""/>
     </header>
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import s from "./Profile.module.css";
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <div>
             <img src="https://palakmishra.files.wordpress.com/2017/06/cropped-image.png?w=676" alt=""/>
         </div>
@@ -13,10 +14,10 @@ const Profile = () => {
             <div>
                 New posts
             </div>
-            <div>
-                <div>post 1</div>
-                <div>post 2</div>
-                <div>post 3</div>
+            <div className={s.posts}>
+                <div className={s.item}>post 1</div>
+                <div className={s.item}>post 2</div>
+                <div className={s.item}>post 3</div>
             </div>
         </div>
     </div>
