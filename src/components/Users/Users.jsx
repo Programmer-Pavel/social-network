741b06ca32e40fb42d48dfdb9e5f66fc20726add
Pavel <pavelkukuskmn388@gmail.com>
2020-11-23 15:08:42 +0300

    Side effects

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,16 +5,19 @@ import photo from '../../assets/images/user.png';
 
 const Users = (props) => {
 
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
-            .then(response => {
-                props.setUsers(response.data.items)
-            })
-    }
+    let getUsers = () => {
 
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+                .then(response => {
+                    props.setUsers(response.data.items)
+                })
+        }
+    }
 
     return (
         <div>
+            <button onClick={getUsers}>getUsers</button>
             {props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
