7c9db867ade14896e8882c9687593535e0201373
Pavel <pavelkukuskmn388@gmail.com>
2020-11-29 22:05:19 +0300

    Preloader, isFetching

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import styles from './users.module.css';
 import photo from '../../assets/images/user.png';
 
--- src/components/redux/users-page-reducer.js
+++ src/components/redux/users-page-reducer.js
@@ -3,14 +3,15 @@ const unfollow = 'unfollow';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
-
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING';
 
 
 let initialState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 0,
-    currentPage: 3
+    currentPage: 3,
+    isFetching: false
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -45,16 +46,21 @@ const usersReducer = (state = initialState, action) => {
             }
 
         case SET_CURRENT_PAGE:
-                    return {
-                        ...state,
-                        currentPage: action.currentPage
-                    }
+            return {
+                ...state,
+                currentPage: action.currentPage
+            }
 
-                    case SET_TOTAL_USERS_COUNT:
-                    return {
-                        ...state,
-                        totalUsersCount: action.count
-                    }
+        case SET_TOTAL_USERS_COUNT:
+            return {
+                ...state,
+                totalUsersCount: action.count
+            }
+        case TOGGLE_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.isFetching
+            }
 
         default:
             return state;
@@ -66,5 +72,6 @@ export const unfollowAC = (userId) => ({type: unfollow, userId});
 export const setUsersAC = (users) => ({type: SET_USERS, users});
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage});
 export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount});
+export const toggleIsFetchingAC = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 
 export default usersReducer;
